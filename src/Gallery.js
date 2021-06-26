a0dca080d52bdc429c0f1bc744d5b05409f7aceb
OdeliaFink <odelia.finkel@gmail.com>
2021-06-26 09:21:44 -0400

    Rendered images

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import Img1 from './Images/Img1.jpeg';
+import Img2 from './Images/Img2.jpeg';
+import Img3 from './Images/Img3.jpeg';
+import Img4 from './Images/Img4.jpeg';
 
-export const Gallery = () => {
-  return <h1>Gallery</h1>;
+const Gallery = () => {
+  let data = [
+    {
+      id: 1,
+      imgSrc: Img1,
+    },
+    {
+      id: 2,
+      imgSrc: Img2,
+    },
+    {
+      id: 3,
+      imgSrc: Img3,
+    },
+    {
+      id: 4,
+      imgSrc: Img4,
+    },
+  ];
+
+  return (
+    <>
+      <div className="gallery">
+        {data.map((item, index) => {
+          return (
+            <div className="pics" key={index}>
+              <img src={item.imgSrc} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Gallery;
