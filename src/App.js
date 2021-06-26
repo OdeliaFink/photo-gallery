4dd05f74a9a0d21f49feea7f895b94be773b80cd
OdeliaFink <odelia.finkel@gmail.com>
2021-06-26 09:05:01 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}>Image Gallery</h1>
+      <h2 style={{ textAlign: 'center' }}>please Subscribe </h2>
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
