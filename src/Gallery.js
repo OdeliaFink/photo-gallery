33cb38764d702b2c45c8bcfddaa68ce4f9005f8a
OdeliaFink <odelia.finkel@gmail.com>
2021-06-26 09:25:01 -0400

    Added styling to gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Gallery.css';
 import Img1 from './Images/Img1.jpeg';
 import Img2 from './Images/Img2.jpeg';
 import Img3 from './Images/Img3.jpeg';
@@ -30,7 +31,7 @@ const Gallery = () => {
         {data.map((item, index) => {
           return (
             <div className="pics" key={index}>
-              <img src={item.imgSrc} />
+              <img src={item.imgSrc} style={{ width: '100%' }} />
             </div>
           );
         })}
